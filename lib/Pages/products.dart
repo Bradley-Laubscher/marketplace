9417cb16084c34a778fbcfd626d4a84945c462dd
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-03-23 08:37:41 +0200

    Added product grid to products page

--- lib/Pages/products.dart
+++ lib/Pages/products.dart
@@ -16,6 +16,24 @@ class ProductsPage extends StatefulWidget {
 }
 
 class _ProductsPageState extends State<ProductsPage> {
+  final TextEditingController _searchController = TextEditingController();
+  List<dynamic> filteredProducts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredProducts = widget.merchant["merchantProducts"];
+  }
+
+  void filterProducts(String query) {
+    setState(() {
+      filteredProducts = widget.merchant["merchantProducts"]
+          .where((product) =>
+          product["productName"].toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
   String checkCurrencyCode() {
     switch (widget.merchant["currencyCode"]) {
       case "710":
@@ -29,113 +47,251 @@ class _ProductsPageState extends State<ProductsPage> {
   Widget build(BuildContext context) {
     List<dynamic> products = widget.merchant["merchantProducts"];
 
-    // **Product Carousel**
-    return Center(
-      child: CarouselSlider(
-        options: CarouselOptions(
-          height: MediaQuery.of(context).size.height*0.5,
-          autoPlay: true,
-          enlargeCenterPage: true,
-        ),
-        items: products.map((product) {
-          return Column(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                  height: 180,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color(widget.merchant["merchantPrimaryColour"]),
-                      width: 2
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: AssetImage(product["productImage"]),
-                      fit: BoxFit.cover,
+    return Column(
+      children: [
+        // **Product Carousel**
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.height * 0.3,
+              autoPlay: true,
+              enlargeCenterPage: true,
+            ),
+            items: products.map((product) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color(widget.merchant["merchantPrimaryColour"]),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(product["productImage"]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    width: 2,
-                    color: Color(widget.merchant["merchantPrimaryColour"]),
-                    style: BorderStyle.solid
-                  )
-                ),
-                width: MediaQuery.of(context).size.width*0.3,
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      product["productName"],
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black87,
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        width: 2,
+                        color: Color(widget.merchant["merchantPrimaryColour"]),
                       ),
                     ),
-                    const SizedBox(height: 4),
-                    Text(
-                      product["productCaption"],
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      "${checkCurrencyCode()}${product["productPrice"]}",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Center(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green[700],
-                          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product["productName"],
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              product["productCaption"],
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              "${checkCurrencyCode()}${product["productPrice"]}",
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ],
+                        ),
+                        // Spacer to push button to the right
+                        const Spacer(),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green[700],
+                              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            onPressed: () {
+                              widget.onAddToCart(product);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('${product["productName"]} added to cart'),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Add to Cart',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                        onPressed: () {
-                          // Add product to cart
-                          widget.onAddToCart(product);
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }).toList(),
+          ),
+        ),
 
-                          // Show a message when product is added
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('${product["productName"]} added to cart')),
-                          );
-                        },
-                        child: const Text(
-                          'Add to Cart',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+        const SizedBox(height: 20),
+
+        // **Filter Input**
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            controller: _searchController,
+            onChanged: filterProducts,
+            decoration: InputDecoration(
+              hintText: 'Search for a product...',
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        // **Products Grid**
+        Expanded(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width*0.6,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: GridView.builder(
+                itemCount: filteredProducts.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, // 2 items per row
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 2,
+                ),
+                itemBuilder: (context, index) {
+                  final product = filteredProducts[index];
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage(product["productImage"]),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    product["productName"],
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    product["productCaption"],
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontStyle: FontStyle.italic,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.grey[700],
+                                    ),
+                                  ),
+                                  Text(
+                                    "${checkCurrencyCode()}${product["productPrice"]}",
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.green[700],
+                                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    widget.onAddToCart(product);
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                            '${product["productName"]} added to cart')),
+                                    );
+                                  },
+                                  child: const Text(
+                                    'Add to Cart',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            ],
-          );
-        }).toList(),
-      ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
